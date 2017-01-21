7fdcc4d9d25a1286bf52bd802b7d2f1283b02e53
Christian Kokott <christian.kokott@gmail.com>
2017-01-21 18:55:27 +0100

    Hole grow cool

--- FluffyCats/Assets/Code/Blackhole.cs
+++ FluffyCats/Assets/Code/Blackhole.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Blackhole : MonoBehaviour {
-
-	// Use this for initialization
+	
 	void Start () {
-		
-	}
+          
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -15,7 +14,10 @@ public class Blackhole : MonoBehaviour {
 	}
 
     void OnTriggerEnter(Collider col) {
-        Destroy( col.gameObject );
-        transform.localScale += new Vector3( 1, 1, 1 );
+        if(col.tag == "Asteroid") {
+            Destroy( col.gameObject );
+
+            iTween.ScaleAdd( gameObject, iTween.Hash( "amount", new Vector3( 1, 1, 1 ), "time", 0.5f, "easetype", iTween.EaseType.easeInOutBack ) );
+        }
     }
 }
