19f2cb704a50306b889ee9334508a8f0710735a8
Christian Kokott <christian.kokott@gmail.com>
2017-01-21 19:41:23 +0100

    Added disabled starfield :(

---
+++ FluffyCats/Assets/Code/Star.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour {
+
+    public float Speed = 5f;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Vector3.back * Speed * Time.deltaTime;
+
+        if(transform.position.z < Camera.main.transform.position.z) {
+            transform.position = new Vector3( transform.position.x, transform.position.y, 0 );
+        }
+	}
+}
---
+++ FluffyCats/Assets/Code/Starfield.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Starfield : MonoBehaviour {
+
+    public GameObject StarPrefab;
+
+    public float MinX = -18f;
+    public float MaxX = 18f;
+
+    public float MinY = 10f;
+    public float MaxY = -10f;
+
+    public int StarCount = 30;
+
+    // Use this for initialization
+    void Start () {
+		for(var i = 0; i <StarCount; i++) {
+            var posX = Random.Range( MinX, MaxX );
+            var posY = Random.Range( MinY, MaxY );
+
+            var star = Instantiate( StarPrefab, this.transform );
+            star.transform.position = new Vector3( posX, posY, 0 );
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
