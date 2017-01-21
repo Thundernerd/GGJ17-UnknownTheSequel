fcc6665b5903196e187312a6a28ff39d4b8d761c
Christian Kokott <christian.kokott@gmail.com>
2017-01-21 18:34:36 +0100

    Black hole grow

--- FluffyCats/Assets/Code/Asteroid.cs
+++ FluffyCats/Assets/Code/Asteroid.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour {
 
+    const double G = 0.0000000000667384;
+
     public Colors Color;
     public float Speed;
 
     public static float Mod;
+    public float BlackHoleMass = 100f;
+
+    public float strengthOfAttraction = 5.0f;
 
     private Vector3 direction;
 
+    private Rigidbody body;
+
     // Use this for initialization
     void Start() {
+        body = GetComponent<Rigidbody>();
+
         Color = (Colors)Random.Range( 0, System.Enum.GetValues( typeof( Colors ) ).Length );
 
         switch ( Color ) {
@@ -34,10 +43,22 @@ public class Asteroid : MonoBehaviour {
         direction.x = Mathf.Cos( angle ) * Mathf.Rad2Deg;
         direction.y = Mathf.Sin( angle ) * Mathf.Rad2Deg;
         direction.Normalize();
+
+        //body = GetComponent<Rigidbody>();
+        //body.AddForce( direction * Speed, ForceMode.Impulse );
     }
 
     // Update is called once per frame
     void Update() {
+        //Vector3 offset = -transform.position;
+        //offset.z = 0;
+        //float magsqr = offset.magnitude;
+        //if ( magsqr > 0.01f ) {
+        //    float attraction = body.mass * BlackHoleMass;
+        //    body.AddForce( attraction * offset.normalized / magsqr );
+            
+        //}            
+
         transform.position += direction * Time.deltaTime * Speed * Mod;
     }
 }
---
+++ FluffyCats/Assets/Code/Blackhole.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blackhole : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider col) {
+        Destroy( col.gameObject );
+        transform.localScale += new Vector3( 1, 1, 1 );
+    }
+}
