e84e7104373dfe41c45eee5529e568ab0586fb5b
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2017-01-22 09:04:35 +0100

    Removed old stuff

--- FluffyCats/Assets/Code/Pusher.cs
+++ FluffyCats/Assets/Code/Pusher.cs
@@ -6,14 +6,6 @@ public class Pusher : MonoBehaviour {
 
     public bool Horizontal;
 
-    private void OnCollisionEnter( Collision collision ) {
-        if ( collision.gameObject.tag.Equals( "Player" ) ) {
-            var n = collision.contacts[0].normal;
-            var d = collision.gameObject.GetComponent<Mover>().Direction;
-            var c = Vector3.Cross( n, d );
-        }
-    }
-
     private void OnTriggerEnter( Collider other ) {
         if ( other.tag.Equals( "Player" ) ) {
             var mover = other.GetComponent<Mover>();
