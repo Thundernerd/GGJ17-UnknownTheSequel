a497ec43f04bb45a62781fea1fa758316685ae9e
Christian Kokott <christian.kokott@gmail.com>
2017-01-22 12:06:54 +0100

    Bloom boom

---
+++ FluffyCats/Assets/MusicBloom.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+public class MusicBloom : MonoBehaviour {
+
+    private Bloom bloom;
+    
+    public int Channel = 0;
+    
+
+	void Start () {
+        bloom = GetComponent<Bloom>();
+	}
+	
+	
+	void Update () {
+        bloom.bloomIntensity = mapRange( Sticker.currentValues[Channel], 0, 0.01f, 2, 7 );
+	}
+
+    float mapRange( float value, float low1, float high1, float low2, float high2 ) {
+        return low2 + ( high2 - low2 ) * ( value - low1 ) / ( high1 - low1 );
+    }
+}
