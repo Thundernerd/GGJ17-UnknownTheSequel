8364eb32f530d1d657d922d468b9d128eefc5e5e
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2017-01-22 10:33:51 +0100

    Better collision

--- FluffyCats/Assets/Code/Pusher.cs
+++ FluffyCats/Assets/Code/Pusher.cs
@@ -26,22 +26,34 @@ public class Pusher : MonoBehaviour {
                     }
                     if ( p2.position.x < transform.position.x ) {
                         var p = p2.position;
-                        p.x += 1;
+                        p.x = transform.position.x + 1;
                         p2.position = p;
                         p2.GetComponent<Mover>().Push( new Vector3( 1, 0, 0 ) );
                     }
                     break;
                 }
             case Sides.Right: {
+                    if ( p1.position.x > transform.position.x ) {
+                        var p = p1.position;
+                        p.x = transform.position.x - 1;
+                        p1.position = p;
+                        p1.GetComponent<Mover>().Push( new Vector3( 1, 0, 0 ) );
+                    }
                     if ( p2.position.x > transform.position.x ) {
                         var p = p2.position;
-                        p.x -= 1;
+                        p.x = transform.position.x - 1;
                         p2.position = p;
                         p2.GetComponent<Mover>().Push( new Vector3( -1, 0, 0 ) );
                     }
                     break;
                 }
             case Sides.Top: {
+                    if ( p1.position.y > transform.position.y ) {
+                        var p = p1.position;
+                        p.y = transform.position.y - 1;
+                        p1.position = p;
+                        p1.GetComponent<Mover>().Push( new Vector3( 0, 1, 0 ) );
+                    }
                     if ( p2.position.y > transform.position.y ) {
                         var p = p2.position;
                         p.y = transform.position.y - 1;
@@ -56,6 +68,12 @@ public class Pusher : MonoBehaviour {
                         p.y = transform.position.y + 1;
                         p1.position = p;
                         p1.GetComponent<Mover>().Push( new Vector3( 0, 1, 0 ) );
+                    }
+                    if ( p2.position.y < transform.position.y ) {
+                        var p = p2.position;
+                        p.y = transform.position.y + 1;
+                        p2.position = p;
+                        p2.GetComponent<Mover>().Push( new Vector3( 0, 1, 0 ) );
                     }
                     break;
                 }
